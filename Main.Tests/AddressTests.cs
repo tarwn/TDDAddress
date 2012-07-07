362b786f536e905fc49378eda1dcf193bc7095d0
Eli Weinstock-Herman <tarwn@tiernok.com>
2012-07-06 22:13:52 -0400

    initial commit

---
+++ Main.Tests/AddressTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Main.Tests {
+
+	[TestFixture]
+	public class AddressTests {
+	}
+}
